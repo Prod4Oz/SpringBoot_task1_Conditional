0156ceb130e1ffec0487ffa6afbc6e7a022a681c
Ivan nout <nn52-ivan@outlook.com>
2022-11-02 15:23:09 +0300

    Initial commit

---
+++ src/main/java/ru/netilogy/conditional/config/JavaConfig.java
@@ -0,0 +1,24 @@
+package ru.netilogy.conditional.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netilogy.conditional.profile.DevProfile;
+import ru.netilogy.conditional.profile.ProductionProfile;
+import ru.netilogy.conditional.profile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
---
+++ src/main/java/ru/netilogy/conditional/profile/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netilogy.conditional.profile;
+
+public interface SystemProfile {
+    String getProfile();
+}
